8a7d28942b9443e0d3c06664b0963e4d02b523dc
noppoman <yuki@miketokyo.com>
2016-08-13 03:07:43 +0900

    Updating to August 4, 2016

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "JSON",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/slimane-swift/StructuredData.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/slimane-swift/Map.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/slimane-swift/MediaType.git", majorVersion: 0, minor: 12)
     ]
 )
---
+++ Sources/JSON.swift
@@ -0,0 +1,10 @@
+//
+//  JSON.swift
+//  JSON
+//
+//  Created by Yuki Takei on 8/13/16.
+//
+//
+
+@_exported import Map
+@_exported import MediaType
